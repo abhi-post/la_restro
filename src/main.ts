c8f65a5ed8ff5b33799f2ecf76e0e2fbe4259668
DESKTOP-QIH79N1\NeoSoft <abhishek.kashyap@neosoftmail.com>
2024-12-18 22:26:22 +0530

    added swager documentation

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true}));
+
+  const config = new DocumentBuilder()
+    .setTitle('La Restro')
+    .setDescription('The La Restro API description')
+    .setVersion('1.0')
+    .addTag('la_restro')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory); 
+
   await app.listen(3001);
 }
 bootstrap();
