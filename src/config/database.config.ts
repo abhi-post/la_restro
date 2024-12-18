283e200a4c852a9b525b69f944da6481a5dc0625
DESKTOP-QIH79N1\NeoSoft <abhishek.kashyap@neosoftmail.com>
2024-12-18 22:05:45 +0530

    env changes

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -5,23 +5,23 @@ import { join } from 'path';
 function typeormModuleOptions(): TypeOrmModuleOptions {
   return {
     type: 'mysql',
-    // host: process.env.DATABASE_HOST,
-    // port: parseInt(process.env.DATABASE_PORT, 10),
-    // username: process.env.DATABASE_USERNAME,
-    // password: process.env.DATABASE_PASSWORD,
-    // database: process.env.DATABASE_NAME,
-    // entities: [join(__dirname, '../**/**/*entity{.ts,.js}')],
-    // autoLoadEntities: true,
-    // synchronize: true
-
-    host: '217.21.94.52',
-    port: 3306,
-    username: 'u306182174_la_restro',
-    password: 'XyE1tC7[uI',
-    database: 'u306182174_la_restro',
+    host: process.env.DATABASE_HOST,
+    port: parseInt(process.env.DATABASE_PORT, 10),
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
     entities: [join(__dirname, '../**/**/*entity{.ts,.js}')],
     autoLoadEntities: true,
     synchronize: false
+
+    // host: '217.21.94.52',
+    // port: 3306,
+    // username: 'u306182174_la_restro',
+    // password: 'XyE1tC7[uI',
+    // database: 'u306182174_la_restro',
+    // entities: [join(__dirname, '../**/**/*entity{.ts,.js}')],
+    // autoLoadEntities: true,
+    // synchronize: false
   }
    
 }
